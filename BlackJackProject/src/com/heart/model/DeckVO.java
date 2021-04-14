f02a3267e3a801dd610dae3038116cb5895571cc
leebin55 <yublee55@gmail.com>
2021-04-14 15:51:39 +0900

    first

---
+++ BlackJackProject/src/com/heart/model/DeckVO.java
@@ -0,0 +1,24 @@
+package com.heart.model;
+
+public class DeckVO {
+
+   String deck ; 
+   Integer value ;
+
+   
+   public String getDeck() {
+	return deck;
+}
+public void setDeck(String deck) {
+	this.deck = deck;
+}
+public Integer getValue() {
+	return value;
+}
+public void setValue(Integer value) {
+	this.value = value;
+}
+   
+   
+   
+}
